8233ba8b2775aa718d8b4bfd95d6764231fc70f7
Elvio Galeano <elvio.galeano827@gmail.com>
2022-08-17 01:09:35 -0300

    funcion de  filtrado de checkbox

--- crearCards.js
+++ crearCards.js
@@ -10,7 +10,7 @@ let cardContainer = document.getElementById("cardContainer");
 /////----- función crear cartas --------//////
 
 function crearCard(e) {
-  card = "";
+  // card = "";
   card = `<div class="cardContainer">
 <div class="card" style="width: 18rem;">
     <img src="${e.imagen}" class="card-img-top" alt="...">
@@ -32,12 +32,28 @@ console.log(arrayCheck);
 
   arrayCheck.forEach((e) => {
     checkContainer.innerHTML += `<div class="form-check form-switch">
-      <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-      <label class="form-check-label" for="flexSwitchCheckDefault">${e}</label>
+      <input class="form-check-input" type="checkbox" role="switch" id="${e}">
+      <label class="form-check-label" for="${e}">${e}</label>
     </div>`;
-
   });
 
+  let checkClickeado = []
+  let todosLosCheckbox = document.querySelectorAll("input[type=checkbox]")
+  function crearCheckbox(){
+    todosLosCheckbox.forEach(checkB => {
+      checkB.addEventListener("click",e =>{
+        let estaChecked = e.target.checked
+        console.log(estaChecked)
+      if(estaChecked){
+        checkClickeado.push(e.target.id)
+      }else{
+        checkClickeado = checkClickeado.filter(uncheck => uncheck !== e.target.id)
+      }
+      console.log(checkClickeado)
+    })
+  })
+}
+  
 
 
 
